f2cf4e7ef4700e400ee31b85cb513cdbf77c65eb
tecrcopentgit <abdulhaadh015@gmail.com>
2026-07-15 22:40:45 +0530

    whatsapp

--- src/components/public-site/WhatsAppButton.tsx
+++ src/components/public-site/WhatsAppButton.tsx
@@ -22,7 +22,7 @@ export default function WhatsAppButton() {
       initial={{ opacity: 0, scale: 0 }}
       animate={{ opacity: 1, scale: 1 }}
       transition={{ delay: 1, duration: 0.3 }}
-      className=" fixed bottom-10 right-10   "
+      className=" fixed bottom-10 right-10 z-20  "
       aria-label="Chat on WhatsApp "
     >
       <div className='p-2 bg-green-400/40 shadow-xl shadow-green-300 hover:bg-green-400 rounded-full h-15 w-15 animate-pulse'><Image src={whatsapp_image} alt=''  className='rounded-full'/></div>
